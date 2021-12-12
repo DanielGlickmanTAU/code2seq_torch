8c544d9a970a6e84de655cda0b705a2ab9320379
Glickman <glickman1@mail.tau.ac.il>
2021-12-12 15:40:37 +0200

    tweak yaml config

--- code2seq/offline_train.py
+++ code2seq/offline_train.py
@@ -3,6 +3,6 @@ import sys
 
 os.system("nohup sh -c '" +
           # sys.executable + " compression_flows.py> res.txt " +
-          # sys.executable + " code2seq_wrapper.py -c ../config/code2seq-py150k.yaml train> training150.txt " +
-          sys.executable + " code2seq_wrapper.py -c ../config/code2seq-py150k-compressed-20.yaml train> training.txt " +
+          sys.executable + " code2seq_wrapper.py -c ../config/code2seq-py150k.yaml train> training150.txt " +
+          # sys.executable + " code2seq_wrapper.py -c ../config/code2seq-py150k-compressed-20.yaml train> training.txt " +
           "' &")
