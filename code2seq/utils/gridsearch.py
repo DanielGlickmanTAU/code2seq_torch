bf6605d1932e667aaf7e7c886dc032a36730fa7e
Glickman <glickman1@mail.tau.ac.il>
2021-12-19 12:22:08 +0200

    slurm

---
+++ code2seq/utils/gridsearch.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+from itertools import product
+
+
+def gridsearch(default_params: Dict, params_to_grid_search: Dict[object, List]) -> List[Dict]:
+    def prod(params_to_grid_search):
+        values = []
+        for v in params_to_grid_search.values():
+            if isinstance(v, list):
+                values.append(v)
+            elif isinstance(v, str):
+                values.append([f'"{v}"'])
+            else:
+                values.append([v])
+        return product(*values)
+
+    params_as_dicts = [dict(zip(params_to_grid_search, v)) for v in prod(params_to_grid_search)]
+    rets = []
+    for d in params_as_dicts:
+        ret = default_params.copy()
+        ret.update(d)
+        rets.append(ret)
+    return rets
---
+++ code2seq/utils/slurm.py
@@ -0,0 +1,38 @@
+import os
+
+import time
+import random
+
+print(os.path)
+python = os.sys.executable
+# partition, time_limit = 'studentkillable', 'infinite'
+partition, time_limit = 'studentbatch', '3-00:00:00'
+slurm_file = 'my_slurm.slurm'
+
+
+def run_on_slurm(job_name, params, slurm=True, gpu=True, sleep=True):
+    python_file = job_name
+    python_file = python_file.replace('.py', '')
+    job_name = job_name + str(time.time())
+    if slurm:
+        with open(slurm_file, 'w') as f:
+            f.write(f'''#! /bin/sh
+#SBATCH --job-name={job_name}
+#SBATCH --output={job_name}.out
+#SBATCH --error={job_name}.err
+#SBATCH -p {partition}
+## SBATCH --time={time_limit}
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus={'1' if gpu else '0'}
+{python} {python_file}.py ''' + ' '.join([f'--{key} {value}' for key, value in params.items()]))
+
+        print(f'executing {job_name} ')
+        os.system(f'sbatch {slurm_file}')
+    else:
+        f = f'{python} {python_file}.py ' + ' '.join([f'--{key} {value}' for key, value in params.items()])
+        os.system(f"nohup sh -c ' {f} > res.txt '&")
+    os.system('chmod 700 slurm.py')
+
+    if sleep:
+        time.sleep(random.randint(0, 15))
