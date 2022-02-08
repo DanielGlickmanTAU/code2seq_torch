fix softmax bug and add test for overfitting small dataset

code2seq/utils/compute.py
@@ -81,7 +81,7 @@ def get_device():
     torch = get_torch()
 
     gpus = get_index_of_free_gpus()
-    print(gpus)
+    # print(gpus)
     return torch.device(compute_gpu_indent(gpus) if torch.cuda.is_available() else 'cpu')
 
 

tests/test_model.py
@@ -1,11 +1,12 @@
+from code2seq.utils import compute
 import argparse
 import unittest
 from unittest import TestCase
 
+import torch
 from torch import optim
 
 from args_parse import get_default_args
-from code2seq.utils import compute
 
 import torch_geometric
 from torch_geometric.data import Data
@@ -21,14 +22,16 @@ from train import train_epoch, evaluate
 
 class Test(TestCase):
 
-    def test_get_spare_x(self):
+    def test_can_overfit_molhiv(self):
+        dataset_samples = 64
         # Training settings
         parser = get_default_args()
         parser.add_argument('--dataset', type=str, default="ogbg-molhiv",
                             help='dataset name (default: ogbg-molhiv)')
         args = parser.parse_args()
         args.attention_type = 'position'
-        args.num_layer = args.num_transformer_layers = 2
+        args.num_layer = args.num_transformer_layers = 4
+        args.drop_ratio = 0.
 
         dataset = PygGraphPropPredDataset(name=args.dataset,
                                           transform=AdjStack(args))
@@ -37,19 +40,29 @@ class Test(TestCase):
         evaluator = Evaluator(args.dataset)
 
         train_loader, valid_loader, test_loader = get_train_val_test_loaders(dataset, num_workers=args.num_workers,
-                                                                             batch_size=args.batch_size, limit=100)
+                                                                             batch_size=args.batch_size, limit=dataset_samples)
         model = get_model(args, dataset.num_tasks, device)
 
-        optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+        optimizer = optim.Adam(model.parameters(), lr=3e-3)
 
-        for epoch in range(1, args.epochs + 1):
+        def hook(model, input, output):
+            if isinstance(output, tuple):
+                output = output[0]
+            assert not output.isnan().any()
+
+
+        torch.nn.modules.module.register_module_forward_hook(hook)
+
+        for epoch in range(1, 100 + 1):
             print("=====Epoch {}".format(epoch))
             print('Training...')
             epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, dataset.task_type)
-
+            print(f'~~~~avg loss {epoch_avg_loss}')
             print('Evaluating...')
             # train_perf = evaluate(model, device, train_loader, evaluator)
-            print(evaluate(model, device, valid_loader, evaluator))
+            rocauc = evaluate(model, device, test_loader, evaluator)['rocauc']
+            print(rocauc)
+        assert rocauc > 0.95
 
 
 if __name__ == '__main__':