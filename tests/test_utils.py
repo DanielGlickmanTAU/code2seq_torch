307ea321651e097e279131a8980b2e3649816bf4
Glickman <glickman1@mail.tau.ac.il>
2022-02-10 09:03:02 +0200

    default arg

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,6 @@ from torch_geometric.data import Dataset, Data
 from torch_geometric.loader import DataLoader
 
 
-def as_pyg_batch(dataset: Union[Dataset, List[Data]], batch_size):
+def as_pyg_batch(dataset: Union[Dataset, List[Data]], batch_size=32):
     loader = DataLoader(dataset, batch_size=batch_size)
     return list(loader)[0]
