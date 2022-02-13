fix content attention masking

ogb-master/examples/graphproppred/mol/model/positional/PositionMultiHeadAttention.py
@@ -96,8 +96,11 @@ class PositionMultiHeadAttention(Module):
         assert self._qkv_same_embed_dim
 
         attention_weights = self.positional_bias(stacks=adj_stack, mask=attn_mask)
+        b, heads, n1, n2 = attention_weights.shape
+        attention_weights = attention_weights.contiguous().view(b * self.num_heads, n1, n1)
         # (n,batch,d)
         value = linear(value, self.in_proj_weight, self.in_proj_bias)
+        attn_mask = pygraph_utils.reshape_attention_mask_to_multihead(attn_mask, self.num_heads)
 
         attn_output, attn_output_weights = multi_head_positional_attention(
             value, attention_weights, self.embed_dim, self.num_heads,
@@ -134,14 +137,11 @@ def multi_head_positional_attention(
     src_len = tgt_len
     head_dim = embed_dim // num_heads
     _assertions(bias_k, bias_v, embed_dim, embed_dim_to_check, head_dim, num_heads,
-                out_proj_bias, out_proj_weight, value, use_separate_proj_weight, key_padding_mask, attention_weights,
-                src_len, bsz)
+                use_separate_proj_weight, key_padding_mask)
 
-    assert attention_weights.shape == (bsz, num_heads, src_len, src_len)
     "expect attention weights to be of shape (batch *num_head,tgt_len,tgt_len)"
-    attn_mask = pygraph_utils.reshape_attention_mask_to_multihead(attn_mask, num_heads)
-    attention_weights = attention_weights.contiguous().view(bsz * num_heads, src_len, src_len)
-
+    assert attention_weights.shape == (bsz * num_heads, src_len, src_len)
+    assert attn_mask.shape == attention_weights.shape
     attn_mask = prep_attention_mask(attn_mask, bsz, num_heads, src_len, tgt_len)
 
     # (batch*num_head, n , d/head)
@@ -173,8 +173,8 @@ def weighted_average(values, attention_weights, attn_mask, training, dropout_p):
         not_nan = ~attn.isnan()
         attn_new = torch.zeros_like(attn, device=attn.device)
         attn_new[not_nan] = attn[not_nan]
-        attn = attn.masked_fill(attn.isnan(), 0)
-        return attn
+        # attn = attn.masked_fill(attn.isnan(), 0)
+        return attn_new
 
     # adjust dropout
     if not training:
@@ -221,8 +221,7 @@ def prep_attention_mask(attn_mask, bsz, num_heads, src_len, tgt_len):
 
 
 def _assertions(bias_k, bias_v, embed_dim, embed_dim_to_check, head_dim, num_heads,
-                out_proj_bias, out_proj_weight, query, use_separate_proj_weight, key_padding_mask, attention_weights,
-                src_len, bsz):
+                use_separate_proj_weight, key_padding_mask):
     assert key_padding_mask is None
 
     assert embed_dim == embed_dim_to_check, \

tests/test_model.py
@@ -10,7 +10,9 @@ from args_parse import get_default_args
 
 from data import dataloader_utils
 from data.dataloader_utils import get_train_val_test_loaders
+from model.ContentMultiHeadAttention import ContentMultiheadAttention
 from model.model_utils import get_model
+from model.positional.PositionMultiHeadAttention import PositionMultiHeadAttention
 from model.positional.positional_attention_weight import AdjStack, AdjStackAttentionWeights
 from ogb.graphproppred import Evaluator, PygGraphPropPredDataset
 from train import train_epoch, evaluate
@@ -18,34 +20,52 @@ from train import train_epoch, evaluate
 
 class Test(TestCase):
 
-    def test_grads_and_outputs(self):
-        dataset_samples = 64
+    def test_grads_and_outputs_content_attention(self):
         # Training settings
         args = get_default_args()
-        args.dataset = "ogbg-molhiv"
+        args.attention_type = 'content'
+        self._assert_output_and_grad(args)
+
+    def test_grads_and_outputs_position_attention(self):
+        def callback(model):
+            def position_attention_backwards_hook(module, grad_input, grad_output):
+                assert not grad_output[0].isnan().any()
+                model.gnn_transformer.transformer.layers[
+                    0].attention_layer.positional_bias.weight.register_full_backward_hook(
+                    position_attention_backwards_hook)
+
+        # Training settings
+        args = get_default_args()
         args.attention_type = 'position'
+        self._assert_output_and_grad(args, callback)
+
+    @staticmethod
+    def assert_not_nan_or_inf(input_tensor):
+        if isinstance(input_tensor, tuple) and input_tensor:
+            input_tensor = input_tensor[0]
+            if isinstance(input_tensor, torch.Tensor):
+                assert not input_tensor.isnan().any()
+                assert not input_tensor.isinf().any()
+
+    def _assert_output_and_grad(self, args, model_callback=None):
+        dataset_samples = 64
+        args.dataset = "ogbg-molhiv"
         args.num_layer = args.num_transformer_layers = 4
         args.drop_ratio = 0.
         args.transformer_encoder_dropout = 0.
-
         dataset = PygGraphPropPredDataset(name=args.dataset,
                                           transform=AdjStack(args))
         device = compute.get_device()
-
         evaluator = Evaluator(args.dataset)
-
         train_loader, valid_loader, test_loader = get_train_val_test_loaders(dataset, num_workers=args.num_workers,
                                                                              batch_size=args.batch_size,
                                                                              limit=dataset_samples)
         model = get_model(args, dataset.num_tasks, device, task='mol')
-
         optimizer = optim.Adam(model.parameters(), lr=3e-4)
 
         def hook(model, input, output):
-            if isinstance(output, tuple):
-                output = output[0]
-            isnan__any = output.isnan().any()
-            assert not isnan__any
+            self.assert_not_nan_or_inf(input)
+            self.assert_not_nan_or_inf(output)
 
         def backward_hook(module, grad_input, grad_output):
             if isinstance(grad_output, tuple):
@@ -58,19 +78,17 @@ class Test(TestCase):
 
             # assert not grad_output.isnan().any()
 
-        def position_attention_backwards_hook(module, grad_input, grad_output):
-            assert not grad_output[0].isnan().any()
-
         torch.nn.modules.module.register_module_forward_hook(hook)
+
         torch.nn.modules.module.register_module_full_backward_hook(backward_hook)
-        model.gnn_transformer.transformer.layers[0].attention_layer.positional_bias.weight.register_full_backward_hook(
-            position_attention_backwards_hook)
+        if model_callback:
+            model_callback(model)
 
         train_epoch(model, device, train_loader, optimizer, dataset.task_type)
         train_epoch(model, device, train_loader, optimizer, dataset.task_type)
         train_epoch(model, device, train_loader, optimizer, dataset.task_type)
 
-    def test_can_overfit_molhiv(self):
+    def test_can_overfit_molhiv_with_positional_attention(self):
         dataset_samples = 64
         # Training settings
         args = get_default_args()
@@ -80,19 +98,30 @@ class Test(TestCase):
         args.drop_ratio = 0.
         args.transformer_encoder_dropout = 0.
 
+        self.assert_overfit(args, dataset_samples)
+
+    def test_can_overfit_molhiv_with_content_attention(self):
+        dataset_samples = 64
+        # Training settings
+        args = get_default_args()
+        args.dataset = "ogbg-molhiv"
+        args.attention_type = 'content'
+        args.num_layer = args.num_transformer_layers = 4
+        args.drop_ratio = 0.
+        args.transformer_encoder_dropout = 0.
+
+        self.assert_overfit(args, dataset_samples)
+
+    def assert_overfit(self, args, dataset_samples):
         dataset = PygGraphPropPredDataset(name=args.dataset,
                                           transform=AdjStack(args))
         device = compute.get_device()
-
         evaluator = Evaluator(args.dataset)
-
         train_loader, valid_loader, test_loader = get_train_val_test_loaders(dataset, num_workers=args.num_workers,
                                                                              batch_size=args.batch_size,
                                                                              limit=dataset_samples)
         model = get_model(args, dataset.num_tasks, device, task='mol')
-
         optimizer = optim.Adam(model.parameters(), lr=3e-4)
-
         for epoch in range(1, 100 + 1):
             epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, dataset.task_type)
 
@@ -153,7 +182,7 @@ class Test(TestCase):
         model = get_model(args, 1, device, task='pattern')
 
         optimizer = optim.Adam(model.parameters(), lr=3e-5)
-        for epoch in range(1, 100 + 1):
+        for epoch in range(1, 10):
             epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, "classification")
 
             print(f'Evaluating epoch {epoch}')