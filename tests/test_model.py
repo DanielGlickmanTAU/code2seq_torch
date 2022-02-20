11f532e6222eb2066ab437a42f2c82ad40486939
Glickman <glickman1@mail.tau.ac.il>
2022-02-20 12:24:41 +0200

    test overfit on TRAIN set

--- tests/test_model.py
+++ tests/test_model.py
@@ -95,10 +95,8 @@ class Test(TestCase):
         args.dataset = "ogbg-molhiv"
         args.attention_type = 'position'
         args.num_layer = args.num_transformer_layers = 4
-        args.drop_ratio = 0.
-        args.transformer_encoder_dropout = 0.
 
-        self.assert_overfit(args, dataset_samples)
+        self.assert_overfit_on_train(args, dataset_samples)
 
     def test_can_overfit_molhiv_with_content_attention(self):
         dataset_samples = 64
@@ -107,10 +105,8 @@ class Test(TestCase):
         args.dataset = "ogbg-molhiv"
         args.attention_type = 'content'
         args.num_layer = args.num_transformer_layers = 4
-        args.drop_ratio = 0.
-        args.transformer_encoder_dropout = 0.
 
-        self.assert_overfit(args, dataset_samples)
+        self.assert_overfit_on_train(args, dataset_samples)
 
     def test_can_overfit_molhiv_with_2_gnn_and_content_attention(self):
         dataset_samples = 64
@@ -120,10 +116,8 @@ class Test(TestCase):
         args.attention_type = 'content'
         args.num_layer = 6
         args.num_transformer_layers = 2
-        args.drop_ratio = 0.
-        args.transformer_encoder_dropout = 0.
         # torch.autograd.set_detect_anomaly(True)
-        self.assert_overfit(args, dataset_samples)
+        self.assert_overfit_on_train(args, dataset_samples)
         # torch.autograd.set_detect_anomaly(False)
 
     def test_can_overfit_molhiv_with_gnn(self):
@@ -133,30 +127,31 @@ class Test(TestCase):
         args.dataset = "ogbg-molhiv"
         args.num_layer = 6
         args.num_transformer_layers = 0
-        args.drop_ratio = 0.
         torch.autograd.set_detect_anomaly(True)
-        self.assert_overfit(args, dataset_samples,score_needed=0.8)
+        self.assert_overfit_on_train(args, dataset_samples, score_needed=0.8)
         torch.autograd.set_detect_anomaly(False)
 
-    def assert_overfit(self, args, dataset_samples, score_needed=0.95):
+    def assert_overfit_on_train(self, args, dataset_samples, score_needed=0.95):
+        args.drop_ratio = 0.
+        args.transformer_encoder_dropout = 0.
+
         dataset = PygGraphPropPredDataset(name=args.dataset,
                                           transform=AdjStack(args))
         device = compute.get_device()
         evaluator = Evaluator(args.dataset)
-        train_loader, valid_loader, test_loader = get_train_val_test_loaders(dataset, num_workers=args.num_workers,
-                                                                             batch_size=args.batch_size,
-                                                                             limit=dataset_samples)
+        train_loader, _, __ = get_train_val_test_loaders(dataset, num_workers=args.num_workers,
+                                                         batch_size=args.batch_size,
+                                                         limit=dataset_samples)
         model = get_model(args, dataset.num_tasks, device, task='mol')
         optimizer = optim.Adam(model.parameters(), lr=3e-4)
         for epoch in range(1, 200 + 1):
             epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, dataset.task_type)
             print(f'loss is {epoch_avg_loss}')
 
-            print(f'Evaluating epoch {epoch}')
-            rocauc = evaluate(model, device, test_loader, evaluator)['rocauc']
+            rocauc = evaluate(model, device, train_loader, evaluator)['rocauc']
             if rocauc > score_needed:
                 break
-            print(rocauc)
+            print(f'Evaluating epoch {epoch}...rocauc: {rocauc}')
         assert rocauc > score_needed
 
     def test_content_position_model_dropout_defaults_to_same_as_overall_dropout(self):
@@ -178,13 +173,14 @@ class Test(TestCase):
         assert model.gnn_transformer.transformer.layers[0].dropout.p != model.gnn_transformer.gnn_node.drop_ratio
 
     def test_pattern_dataset(self):
-        dataset_samples = 64
+        dataset_samples = 32 * 100
         # Training settings
         args = get_default_args()
         args.dataset = "PATTERN"
         args.attention_type = 'position'
-        args.num_layer = args.num_transformer_layers = 4
+        args.num_layer = args.num_transformer_layers = 2
         args.drop_ratio = 0.
+        # todo need better embedding...! now using strange embedding for atoms
         args.emb_dim = 3
         args.num_heads = 1
 
@@ -209,13 +205,13 @@ class Test(TestCase):
         model = get_model(args, 1, device, task='pattern')
 
         optimizer = optim.Adam(model.parameters(), lr=3e-5)
-        for epoch in range(1, 10):
-            epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, "classification")
+        for epoch in range(1, 20):
+            epoch_avg_loss = train_epoch(model, device, train_loader, optimizer, "node classification")
 
             print(f'Evaluating epoch {epoch}')
             print(evaluate(model, device, test_loader, evaluator))
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    Test().test_can_overfit_molhiv_with_1_gnn_and_content_attention()
+    unittest.main()
+    # Test().test_can_overfit_molhiv_with_1_gnn_and_content_attention()
