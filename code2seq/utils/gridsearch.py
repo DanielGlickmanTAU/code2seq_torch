c70f064f279420763fbf01b5d7d57239cb61faeb
Glickman <glickman1@mail.tau.ac.il>
2022-02-23 16:30:01 +0200

    handle tuples in grid search

--- code2seq/utils/gridsearch.py
+++ code2seq/utils/gridsearch.py
@@ -14,7 +14,21 @@ def gridsearch(default_params: Dict, params_to_grid_search: Dict[object, List]) 
                 values.append([v])
         return product(*values)
 
-    params_as_dicts = [dict(zip(params_to_grid_search, v)) for v in prod(params_to_grid_search)]
+    def flatten_tuples(d):
+        ret = {}
+        for k, v in d.items():
+            if isinstance(k, tuple):
+                k_1, k_2 = k
+                v_1, v_2 = v
+                ret[k_1] = v_1
+                ret[k_2] = v_2
+            else:
+                ret[k] = v
+        return ret
+
+    params_as_dicts = [dict(zip(params_to_grid_search.keys(), v)) for v in prod(params_to_grid_search)]
+    params_as_dicts = [flatten_tuples(d) for d in params_as_dicts]
+
     rets = []
     for d in params_as_dicts:
         ret = default_params.copy()
