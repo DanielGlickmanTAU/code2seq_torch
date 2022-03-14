59680ed4b7ff4fba588415f167cb8a6912f8f2ee
Glickman <glickman1@mail.tau.ac.il>
2022-03-14 13:10:43 +0200

    try to fix comet throttling by not printing all weights

--- ogb-master/examples/graphproppred/mol/train/training.py
+++ ogb-master/examples/graphproppred/mol/train/training.py
@@ -24,16 +24,16 @@ def visualize_activations_and_grads(model, exp):
     for name, param in model.named_parameters():
         weight_norm = calc_norm(param.data)
         grad_norm = calc_norm(param.grad)
-        print(name)
-        print(f'weight norm: {weight_norm}')
-        print(f'grad norm: {grad_norm}')
         grad_magnitude = (grad_norm / weight_norm) if (weight_norm is not None and grad_norm is not None) else None
-        print(
-            f' grad norm/weight norm: {grad_magnitude}')
         if exp:
             exp.log_metric(value=weight_norm, name=f'{name}:weight_norm')
             exp.log_metric(value=grad_norm, name=f'{name}:grad_norm')
             exp.log_metric(value=grad_magnitude, name=f'{name}:grad/weight')
+        else:
+            print(name)
+            print(f'weight norm: {weight_norm}')
+            print(f'grad norm: {grad_norm}')
+            print(f' grad norm/weight norm: {grad_magnitude}')
         print('_________________')
     print('----END ACTIVATIONS------')
 
