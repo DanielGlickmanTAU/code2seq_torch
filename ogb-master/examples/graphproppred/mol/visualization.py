fed6437846de83d038c2a926d1f28ce00ef098ec
Glickman <glickman1@mail.tau.ac.il>
2022-03-21 18:02:23 +0200

    take care of visualizations when no label

--- ogb-master/examples/graphproppred/mol/visualization.py
+++ ogb-master/examples/graphproppred/mol/visualization.py
@@ -7,7 +7,7 @@ import torch_geometric
 def draw_pyg_graph(graph: torch_geometric.data.Data, to_undirected=True):
     nx_graph = torch_geometric.utils.to_networkx(data=graph, to_undirected=to_undirected)
     nx.draw(nx_graph,
-            node_color=['red' if y == 1 else 'blue' for y in graph.y],
+            node_color=['red' if y == 1 else 'blue' for y in graph.y] if hasattr(graph, 'y') else None,
             # labels={key: (value['type'] if 'type' in value else value['value']) for key, value in
             #         ast.items()},
             # with_labels=True
@@ -32,5 +32,5 @@ def get_edges(graph: torch_geometric.data.Data):
 #            edges = get_edges(train_loader.dataset[0])
 #             mean_std_by_labels = {key: (len(value), value.mean(0).tolist(), value.std(0).tolist()) for key, value in
 #                                   edges.items()}
-#p[y neibour | y self] = {key: value[:,1].sum() / (edges[(key[0],1-key[1])][:,1].sum()   + value[:,1].sum()  ) for key,value in edges.items() }
-#p[connect | (y_neigbour,y_self) = {key: value[:,1].sum() / ( len( edges[(key[0],1-key[1])]  ) + len(value)  ) for key,value in edges.items() }
+# p[y neibour | y self] = {key: value[:,1].sum() / (edges[(key[0],1-key[1])][:,1].sum()   + value[:,1].sum()  ) for key,value in edges.items() }
+# p[connect | (y_neigbour,y_self) = {key: value[:,1].sum() / ( len( edges[(key[0],1-key[1])]  ) + len(value)  ) for key,value in edges.items() }
