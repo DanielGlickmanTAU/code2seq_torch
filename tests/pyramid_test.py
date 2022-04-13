249f30c5898c8285137d325f1914051b49402d5e
Glickman <glickman1@mail.tau.ac.il>
2022-04-13 17:47:33 +0300

    draw stuff

--- tests/pyramid_test.py
+++ tests/pyramid_test.py
@@ -1,5 +1,7 @@
 import collections
 
+import tabulate
+
 from code2seq.utils import compute
 
 torch = compute.get_torch()
@@ -34,9 +36,11 @@ def draw_attention(graph, source_node, attention_matrix):
     source_node_attention_scores_nx_indexed = {tensor_id_to_nx_index[i]: score.item() for i, score in
                                                enumerate(source_node_attention_scores)}
     heatmap = [source_node_attention_scores_nx_indexed[n] for n in graph.nodes]
-    nx.draw(graph, positions, node_color=heatmap, with_labels=True)
+
+    nx.draw(graph, positions, node_color=heatmap, with_labels=True, cmap=plt.cm.Reds)
     nx.draw(graph.subgraph(source_node), positions, node_color=[heatmap[source_node_tensor_index]],
-            with_labels=True, font_color='red')
+            with_labels=True, font_color='red', cmap=plt.cm.Reds)
+
     plt.show()
 
 
@@ -66,44 +70,26 @@ def map_tensor_edge_to_networkx_node_ids(graph, edges):
 
 
 min_row_size = 1
-max_row_size = 6
-num_adj_stacks = 3
+max_row_size = 10
+num_adj_stacks = 5
 
 graph, positions = examples.create_pyramid(max_row_size, min_row_size)
+nx_id_to_tensor_index = {x: i for i, x in enumerate(graph.nodes())}
+tensor_id_to_nx_index = {i: x for i, x in enumerate(graph.nodes())}
 
 color_graph(graph)
 colors = [index_to_color[graph.nodes[x]['color']] for x in graph.nodes]
+
 nx.draw(graph, positions, node_color=colors, with_labels=True)
 plt.show()
 
 data = torch_geometric.utils.from_networkx(graph, all)
 stacks = create_stacks(num_adj_stacks)
-
 stacks = stacks.permute(1, 2, 0)
-
 edges_to_nodes_tuple, edges_to_is_same_color = map_tensor_edge_to_networkx_node_ids(graph, stacks)
 
-
-def calc_stats(edges_to_nodes_tuple, edges_to_is_same_color):
-    edges_that_connect_same_color_and_also_different_color = [k for k, v in edges_to_is_same_color.items() if
-                                                              len(set(v)) > 1]
-    print(f'num unique edges: {len(edges_to_nodes_tuple)}')
-    print(
-        f'num edges that exist between both nodes of same color and different color: {len(edges_that_connect_same_color_and_also_different_color)}')
-    num_all_pairs = sum([len(x) for x in edges_to_nodes_tuple.values()])
-    ambigious_count = len(edges_to_nodes_tuple[(0, 0, 0)])
-    reachable_percent = 1. - ambigious_count / num_all_pairs
-    # edges_that_connect_same_color_and_also_different_color.remove((0, 0, 0))
-    num_really_ambigious = sum(
-        [len(edges_to_nodes_tuple[x]) for x in edges_that_connect_same_color_and_also_different_color if
-         x != (0, 0, 0)])
-    print(f'reachble percent {reachable_percent}')
-    print(f'num really ambigious {num_really_ambigious}')
-
-
-calc_stats()
-
 stacks_batch = torch.stack([stacks.permute(2, 0, 1)])
+
 adj_bias_model = AdjStackAttentionWeights(num_adj_stacks, num_heads=1, bias=False)
 
 edge_to_relative_edge_weight = {edge: sum(same_color) / len(same_color) for edge, same_color in
@@ -122,6 +108,18 @@ adj_bias_model.weight = test_utils.MockModule(mock_edge_weight)
 
 new_stacks = adj_bias_model(stacks_batch).squeeze()
 
+edge_string = lambda e1, e2: ["%.3f" % x for x in stacks[nx_id_to_tensor_index[e1], nx_id_to_tensor_index[e2]].tolist()]
+
+fig, axe = plt.subplots(figsize=(12, 7))
+title = f'\n(4,2)->(7,5):{edge_string((4, 2), (7, 5))}    ' \
+        f'(4,2)->(7,4):{edge_string((4, 2), (7, 4))}\n' \
+        f'(8,7)->(9,9):{edge_string((9, 7), (9, 9))}    ' \
+        f'(8,7)->(6,6):{edge_string((9, 7), (6, 6))}' \
+        f''
+axe.set_title(title, loc='right')
+nx.draw(graph, positions, node_color=colors, ax=axe, with_labels=True)
+plt.show()
+
 source_node = (2, 1)
-draw_attention(graph, source_node, -new_stacks.softmax(dim=-1))
-draw_attention(graph, source_node, -new_stacks.softmax(dim=-1) @ (new_stacks.T))
+draw_attention(graph, source_node, new_stacks.softmax(dim=-1))
+draw_attention(graph, source_node, new_stacks.softmax(dim=-1) @ (new_stacks))
