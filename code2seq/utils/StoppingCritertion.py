6b232a732e964bc9c65edebe5f8cab7db2a8cbe9
Glickman <glickman1@mail.tau.ac.il>
2022-04-20 10:58:41 +0300

    stopping

--- code2seq/utils/StoppingCritertion.py
+++ code2seq/utils/StoppingCritertion.py
@@ -1,11 +1,12 @@
 class StoppingCriterion:
-    def __init__(self, threshold=None, desired_stable_evaluations=2, higher_is_better=True):
+    def __init__(self, threshold=None, desired_stable_evaluations=2, patience=None, higher_is_better=True):
         self.threshold = threshold
         self.desired_stable_evaluations = desired_stable_evaluations
+        self.patience = patience
         self.higher_is_better = higher_is_better
         self.num_consecutive_successes = 0
         self.num_no_improvement = 0
-        self.last_result = float('-inf') if higher_is_better else float('inf')
+        self.best_so_far = float('-inf') if higher_is_better else float('inf')
 
     def __call__(self, result):
         if self.threshold:
@@ -14,8 +15,15 @@ class StoppingCriterion:
             else:
                 self.num_consecutive_successes = 0
 
-        self.last_result = result
-        return self.num_consecutive_successes >= self.desired_stable_evaluations
+        if self.is_better(self.best_so_far, result):
+            self.num_no_improvement += 1
+        else:
+            self.num_no_improvement = 0
+            self.best_so_far = result
+
+        patience_critirion = self.patience and self.num_no_improvement > self.patience
+        thershold_critiertion = self.desired_stable_evaluations and self.num_consecutive_successes >= self.desired_stable_evaluations
+        return thershold_critiertion or patience_critirion
 
     def is_better(self, new_value, old_value):
         if self.higher_is_better:
--- tests/test_StoppingCritertion.py
+++ tests/test_StoppingCritertion.py
@@ -20,3 +20,18 @@ class TestStoppingCriterion(TestCase):
 
         critertion(1.)
         assert critertion(1.)
+
+    def test_early_stop(self):
+        critertion = StoppingCriterion(higher_is_better=True, patience=1)
+        assert not critertion(0.1)
+        assert not critertion(0.2)
+        assert not critertion(0.3)
+
+        # not updating first time
+        assert not critertion(0.3)
+        # now updating should reset counter
+        assert not critertion(0.4)
+        # not updating again
+        assert not critertion(0.4)
+        # second time with no improvement, should stop
+        assert critertion(0.4)
