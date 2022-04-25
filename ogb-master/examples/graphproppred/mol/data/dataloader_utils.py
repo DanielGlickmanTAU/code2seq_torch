644f4044b48c1b97307726a8d1a5191863aa93ad
Glickman <glickman1@mail.tau.ac.il>
2022-04-25 11:34:27 +0300

    refactor stuff

--- ogb-master/examples/graphproppred/mol/data/dataloader_utils.py
+++ ogb-master/examples/graphproppred/mol/data/dataloader_utils.py
@@ -18,27 +18,23 @@ def get_train_val_test_loaders(dataset: GNNBenchmarkDataset, batch_size, num_wor
     return train_loader, valid_loader, test_loader
 
 
+def create_dataset_loader(dataset, batch_size, num_workers=0, limit=0, mapping=None, shuffle=True):
+    dataset = dataset[: limit] if limit else dataset
+    if mapping:
+        dataset = [mapping(x) for x in dataset]
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                      num_workers=num_workers)
+
+
 def pyg_get_train_val_test_loaders(name, batch_size, num_workers=0, transform=None, limit=0, mapping=None
                                    ):
     train_dataset = GNNBenchmarkDataset(root=('./%s' % name), name=('%s' % name), split='train', transform=transform)
     val_dataset = GNNBenchmarkDataset(root=name, name=('%s' % name), split='val', transform=transform)
     test_dataset = GNNBenchmarkDataset(root=name, name=('%s' % name), split='test', transform=transform)
 
-    train_dataset = train_dataset[: limit] if limit else train_dataset
-    val_dataset = val_dataset[: limit] if limit else val_dataset
-    test_dataset = test_dataset[: limit] if limit else test_dataset
-
-    if mapping:
-        train_dataset = [mapping(x) for x in train_dataset]
-        val_dataset = [mapping(x) for x in val_dataset]
-        test_dataset = [mapping(x) for x in test_dataset]
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                              num_workers=num_workers)
-    valid_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
-                              num_workers=num_workers)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
-                             num_workers=num_workers)
+    train_loader = create_dataset_loader(train_dataset, batch_size, num_workers, limit, mapping, shuffle=True)
+    valid_loader = create_dataset_loader(val_dataset, batch_size, num_workers, limit, mapping, shuffle=False)
+    test_loader = create_dataset_loader(test_dataset, batch_size, num_workers, limit, mapping, shuffle=False)
     return train_loader, valid_loader, test_loader
 
 
