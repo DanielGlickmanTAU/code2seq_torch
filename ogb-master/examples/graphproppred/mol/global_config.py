15cfc1d9470fa0bbc401e7f537be52a0d5c238e6
Glickman <glickman1@mail.tau.ac.il>
2022-04-27 10:32:36 +0300

    disable printing too much stuff

--- ogb-master/examples/graphproppred/mol/global_config.py
+++ ogb-master/examples/graphproppred/mol/global_config.py
@@ -1,7 +1,7 @@
 # variables used mostly for debugging
 
 log_train_acc = True
-print_parameter_norms = True
+print_parameter_norms = False
 print_position_weights = False
 mask_far_away_nodes = True
 normalize_by_n = False
