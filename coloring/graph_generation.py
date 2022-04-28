90b22566625b3f2e6e688d902ddbafeb7ce408e0
Glickman <glickman1@mail.tau.ac.il>
2022-04-28 09:54:36 +0300

    wip

--- coloring/graph_generation.py
+++ coloring/graph_generation.py
@@ -18,9 +18,10 @@ def create_pyramid(min_row_size, max_row_size):
     return graph, positions
 
 
-def create_hexagon_from_triangles(max_row_size, min_row_size):
-    graph, positions = create_pyramid(min_row_size, max_row_size)
+def create_hexagon_from_triangles(min_row_size, max_row_size):
+    graph, _ = create_pyramid(min_row_size, max_row_size)
     # lower graph part:
+    #todo play with + 1 here
     for row in range(max_row_size + 1, 2 * max_row_size - min_row_size + 1):
         row_size = 2 * max_row_size - row
         for col in range(1, row_size):
@@ -28,5 +29,6 @@ def create_hexagon_from_triangles(max_row_size, min_row_size):
         for col in range(row_size):
             graph.add_edge((row, col), (row - 1, col))
             graph.add_edge((row, col), (row - 1, col + 1))
-    #todo fix positions offset to start at 0
+
+    positions = {(row, col): (col + 0.5 * abs(row - max_row_size), -row) for (row, col) in graph.nodes}
     return graph, positions
