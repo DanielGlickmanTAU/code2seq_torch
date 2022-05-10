d525676bf2458f523e6e8987888d381089419a33
Glickman <glickman1@mail.tau.ac.il>
2022-05-10 10:52:35 +0300

    dataset for joining graphs..

--- graph_words/word_graphs.py
+++ graph_words/word_graphs.py
@@ -18,12 +18,16 @@ def _create_clique(n):
 def Clique(n):
     graph = _create_clique(n)
     graph.name = f'{n}_clique'
+    for node in graph.nodes:
+        graph.nodes[node]['color'] = graph.name
     return graph
 
 
 def Cycle(n):
     graph = nx.cycle_graph(n)
     graph.name = f'{n}_cycle'
+    for node in graph.nodes:
+        graph.nodes[node]['color'] = graph.name
     return graph
 
 
@@ -42,26 +46,43 @@ class WordGraphDataset(Dataset):
         self.name_2_label = name_2_label
         self.dataset = []
         for graph in basic_graphs:
-            pyg_graph = self.create_pyg_graph(graph)
+            pyg_graph = create_pyg_graph(graph)
             self.dataset.append(pyg_graph)
-
-    def create_pyg_graph(self, graph):
-        pyg_graph = torch_geometric.utils.from_networkx(graph)
-        pyg_graph.x = torch.zeros((pyg_graph.num_nodes,), dtype=torch.long)
-        y_value = name_2_label[graph.name]
-        pyg_graph.y = torch.full_like(pyg_graph.x, y_value)
-
-        N = len(graph.nodes)
-        graph.positions = {i: (i if i < N / 2 else N - i, 1 if i < N / 2 else -1) for i, x in enumerate(
-            graph.nodes)}
-        pyg_graph.graph = graph
-        return pyg_graph
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
         return self.dataset[idx]
+
+
+class WordsCombinationGraphDataset(Dataset):
+    def __init__(self):
+        self.name_2_label = name_2_label
+        self.dataset = []
+        graph = join_graphs([cycle_4, clique_5])
+        pyg_graph = create_pyg_graph(graph)
+        self.dataset.append(pyg_graph)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+
+def create_pyg_graph(graph):
+    pyg_graph = torch_geometric.utils.from_networkx(graph)
+    pyg_graph.x = torch.zeros((pyg_graph.num_nodes,), dtype=torch.long)
+    y_value = name_2_label[graph.name]
+    # pyg_graph.y = torch.full_like(pyg_graph.x, y_value)
+    pyg_graph.y = torch.tensor([name_2_label[attr['color']] for _, attr in graph.nodes(data=True)])
+
+    N = len(graph.nodes)
+    graph.positions = {i: (i if i < N / 2 else N - i, 1 if i < N / 2 else -1) for i, x in enumerate(
+        graph.nodes)}
+    pyg_graph.graph = graph
+    return pyg_graph
 
 
 def join_graphs(graphs):
---
+++ graph_words/words_combinations_main.py
@@ -0,0 +1,58 @@
+from code2seq.utils import compute
+
+import networkx as nx
+import torch_geometric
+
+import visualization
+from args_parse import get_default_args
+from data import dataloader_utils
+from graph_words import word_graphs
+from model import model_utils
+from model.positional.positional_attention_weight import AdjStack
+from ogb.graphproppred import Evaluator
+from train import training
+
+dataset = word_graphs.WordsCombinationGraphDataset()
+
+# # draw graphs, each should have different color(by label)
+# for pyg_graph in dataset:
+#     visualization.draw(pyg_graph, pyg_graph.y, color_map={0: 'pink', 1: 'red', 2: 'blue', 3: 'green', 4: 'yellow'})
+# # color with x(no label) just as sanity
+# visualization.draw(pyg_graph, pyg_graph.x, color_map={0: 'gray'})
+
+# overfit train
+args = get_default_args()
+
+torch_geometric.seed_everything(args.seed)
+
+# num_adj_stacks = pyramid_size - 1
+
+args.num_transformer_layers = 0
+args.num_layer = 6
+args.drop_ratio = 0.
+args.transformer_encoder_dropout = 0.
+args.emb_dim = 100
+args.num_heads = 1
+args.patience = 400
+args.epochs = 2000
+# args.lr_schedule_patience = 500
+args.lr_reduce_factor = 0.9
+num_colors = len(dataset.name_2_label)
+device = compute.get_device()
+task = 'coloring'
+model = model_utils.get_model(args, num_tasks=num_colors, device=device, task=task, num_embedding=num_colors + 1)
+evaluator = Evaluator('coloring')
+loader = dataloader_utils.create_dataset_loader(dataset, batch_size=64, mapping=AdjStack(args))
+valid_loader = dataloader_utils.create_dataset_loader(dataset, batch_size=64, mapping=AdjStack(args), shuffle=False)
+test_loader = dataloader_utils.create_dataset_loader(dataset, batch_size=32, mapping=AdjStack(args),
+                                                     shuffle=False)
+# exp = None
+# exp = exp_utils.start_exp(args.exp_name, args, model)
+# test_flow_utils.train_and_assert_overfit(model, loader, evaluator, 'coloring', exp=exp, test_loader=test_loader)
+
+training.full_train_flow(args, device, evaluator, model, test_loader, loader, valid_loader, 'coloring',
+                         'acc')
+
+# join graphs
+
+# join edges(last node to first node...)
