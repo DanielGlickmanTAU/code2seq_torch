0d4cc35f0534a32f1fee5a2c4000a9b3ef207deb
Glickman <glickman1@mail.tau.ac.il>
2022-05-10 07:51:27 +0300

    extract visualizer class

---
+++ ogb-master/examples/graphproppred/mol/Visualizer.py
@@ -0,0 +1,47 @@
+import visualization
+from exp_utils import get_global_exp
+
+
+class Visualizer:
+    def __init__(self, task_name, epoch, graph_indexes=[0, 1], label_names=None):
+        self.label_names = label_names
+        self.epoch = epoch
+        self.task_type = task_name
+        self.graph_indexes = graph_indexes
+
+    @staticmethod
+    def _lexo(n, max_val):
+        s = str(n)
+        n = n * 10
+        while n <= max_val:
+            s = '0' + s
+            n = n * 10
+        return s
+
+    def __call__(self, graphs, y_true, y_pred):
+        if not self.task_type == 'coloring':
+            return
+
+        graph_to_prediction_indexes = {}
+        predictions_start = 0
+        for i, graph in enumerate(graphs):
+            predictions_end = predictions_start + graph.num_nodes
+            graph_to_prediction_indexes[i] = (predictions_start, predictions_end)
+            predictions_start = predictions_end
+
+        for index in self.graph_indexes:
+            try:
+                g = graphs[index]
+
+                pred_start, pred_end = graph_to_prediction_indexes[index]
+                g_pred = y_pred[pred_start:pred_end]
+                g_acc = (g_pred.argmax(dim=-1) == g.y).float().mean()
+                g_acc = str(round(g_acc.item(), 2))
+                if self.epoch == 1:
+                    visualization.draw_pyramid(g, 'x', 'input')
+                    visualization.draw_pyramid(g, 'y', 'gold')
+                visualization.draw_pyramid(g, g_pred, f'epoch {self._lexo(self.epoch, 1000)}. acc:{g_acc}')
+            except Exception as e:
+                print(f'failed visualizing {e}')
+
+        get_global_exp().log_confusion_matrix(y_true.numpy(), y_predicted=y_pred.numpy(), labels=self.label_names)
--- ogb-master/examples/graphproppred/mol/train/training.py
+++ ogb-master/examples/graphproppred/mol/train/training.py
@@ -1,6 +1,6 @@
 import global_config
-import visualization
-from exp_utils import start_exp, get_global_exp
+from Visualizer import Visualizer
+from exp_utils import start_exp
 import numpy as np
 from torch import optim as optim
 from tqdm import tqdm
@@ -92,51 +92,6 @@ def _assert_no_zero_grad(model):
     for name, p in model.named_parameters():
         if (p.grad == 0).all():
             raise Exception(f'found all zero grads in {name}, shape: {p.shape}')
-
-
-class Visualizer:
-    def __init__(self, task_name, epoch, graph_indexes=[0, 1], label_names=None):
-        self.label_names = label_names
-        self.epoch = epoch
-        self.task_type = task_name
-        self.graph_indexes = graph_indexes
-
-    @staticmethod
-    def _lexo(n, max_val):
-        s = str(n)
-        n = n * 10
-        while n <= max_val:
-            s = '0' + s
-            n = n * 10
-        return s
-
-    def __call__(self, graphs, y_true, y_pred):
-        if not self.task_type == 'coloring':
-            return
-
-        graph_to_prediction_indexes = {}
-        predictions_start = 0
-        for i, graph in enumerate(graphs):
-            predictions_end = predictions_start + graph.num_nodes
-            graph_to_prediction_indexes[i] = (predictions_start, predictions_end)
-            predictions_start = predictions_end
-
-        for index in self.graph_indexes:
-            try:
-                g = graphs[index]
-
-                pred_start, pred_end = graph_to_prediction_indexes[index]
-                g_pred = y_pred[pred_start:pred_end]
-                g_acc = (g_pred.argmax(dim=-1) == g.y).float().mean()
-                g_acc = str(round(g_acc.item(), 2))
-                if self.epoch == 1:
-                    visualization.draw_pyramid(g, 'x', 'input')
-                    visualization.draw_pyramid(g, 'y', 'gold')
-                visualization.draw_pyramid(g, g_pred, f'epoch {self._lexo(self.epoch, 1000)}. acc:{g_acc}')
-            except Exception as e:
-                print(f'failed visualizing {e}')
-
-        get_global_exp().log_confusion_matrix(y_true.numpy(), y_predicted=y_pred.numpy(), labels=self.label_names)
 
 
 def full_train_flow(args, device, evaluator, model, test_loader, train_loader, valid_loader, task_type, eval_metric):
