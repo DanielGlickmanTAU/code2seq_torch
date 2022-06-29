b1f4b1ed0316a219168fc3315027d3811bf2d244
Glickman <glickman1@mail.tau.ac.il>
2022-06-29 10:29:37 +0300

    handle laplace encoder dim_ff just in case

--- GraphGPS/graphgps/encoder/laplace_pos_encoder.py
+++ GraphGPS/graphgps/encoder/laplace_pos_encoder.py
@@ -51,6 +51,7 @@ class LapPENodeEncoder(torch.nn.Module):
         if model_type == 'Transformer':
             # Transformer model for LapPE
             encoder_layer = nn.TransformerEncoderLayer(d_model=dim_pe,
+                                                       dim_feedforward=4 * dim_pe,
                                                        nhead=n_heads,
                                                        batch_first=True)
             self.pe_encoder = nn.TransformerEncoder(encoder_layer,
@@ -87,7 +88,6 @@ class LapPENodeEncoder(torch.nn.Module):
                 layers.append(nn.ReLU())
             self.post_mlp = nn.Sequential(*layers)
 
-
     def forward(self, batch):
         if not (hasattr(batch, 'EigVals') and hasattr(batch, 'EigVecs')):
             raise ValueError("Precomputed eigen values and vectors are "
@@ -102,7 +102,7 @@ class LapPENodeEncoder(torch.nn.Module):
             sign_flip[sign_flip < 0.5] = -1.0
             EigVecs = EigVecs * sign_flip.unsqueeze(0)
 
-        pos_enc = torch.cat((EigVecs.unsqueeze(2), EigVals), dim=2) # (Num nodes) x (Num Eigenvectors) x 2
+        pos_enc = torch.cat((EigVecs.unsqueeze(2), EigVals), dim=2)  # (Num nodes) x (Num Eigenvectors) x 2
         empty_mask = torch.isnan(pos_enc)  # (Num nodes) x (Num Eigenvectors) x 2
 
         pos_enc[empty_mask] = 0  # (Num nodes) x (Num Eigenvectors) x 2
