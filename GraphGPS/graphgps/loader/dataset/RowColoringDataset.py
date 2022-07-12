ad22028513cd77a4fbc554bbbf91001350967c7c
danielg <glickman1@mail.tau.ac.il>
2022-07-12 16:59:13 +0300

    allow changing row and cols sizes

--- GraphGPS/graphgps/loader/dataset/RowColoringDataset.py
+++ GraphGPS/graphgps/loader/dataset/RowColoringDataset.py
@@ -17,6 +17,12 @@ class RowColoringDataset(InMemoryDataset):
         if 'atom_set' in cfg and cfg['atom_set']:
             atom_set = cfg['atom_set']
 
+        if 'num_rows' in cfg and cfg['num_rows'] > 0:
+            words_per_row = cfg['words_per_row']
+            num_rows = cfg['num_rows']
+        else:
+            num_rows = words_per_row = 4
+
         only_color = cfg.dataset.only_color
         ds = WordsCombinationGraphDataset(color_mode='rows',
                                           word_graphs=graph_words.word_graphs.get_atom_set(atom_set),
@@ -24,9 +30,10 @@ class RowColoringDataset(InMemoryDataset):
                                           num_colors=cfg.dataset.node_encoder_num_types,
                                           only_color=only_color,
                                           unique_colors_per_example=True,
-                                          #if tagging by shape, draw unique shape per instace, to get reasonable probability.
+                                          # if tagging by shape, draw unique shape per instace, to get reasonable probability.
                                           unique_atoms_per_example=not only_color,
-                                          words_per_row=4
+                                          words_per_row=words_per_row,
+                                          num_rows=num_rows
                                           )
         self.data, self.slices = self.collate(ds.dataset)
         # in WOrdCombinationGraphDataset x gets values from 1 to num_colors(because of some issue with drawing the graph colors).. but here we want it to get values from 0 to num_colors-1
