d1e79fbaccf97d8f7bb4a303eef4bdd8a5534d4a
danielg <glickman1@mail.tau.ac.il>
2022-07-28 09:35:59 +0300

    profile

--- GraphGPS/custom/config.py
+++ GraphGPS/custom/config.py
@@ -31,6 +31,7 @@ def parse_args():
     parser.add_argument('--num_unique_atoms', type=int, default=2)
     parser.add_argument('--num_unique_colors', type=int, default=2)
     parser.add_argument('--make_prob_of_row_half', type=bool_, default=False)
+    parser.add_argument('--profiler', type=bool_, default=False)
 
     parser.add_argument('opts', help='See graphgym/config.py for all options',
                         default=None, nargs=argparse.REMAINDER)
