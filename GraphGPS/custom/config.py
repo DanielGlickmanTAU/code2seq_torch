aeb56eae4fb54d6b52d2de571e3250df31e2a580
danielglickman <glickman1@mail.tau.ac.il>
2022-08-16 09:21:20 +0300

    row or setting

--- GraphGPS/custom/config.py
+++ GraphGPS/custom/config.py
@@ -33,6 +33,8 @@ def parse_args():
     parser.add_argument('--make_prob_of_row_half', type=bool_, default=False)
     parser.add_argument('--shape_per_row', type=bool_, default=False)
     parser.add_argument('--color_per_row', type=bool_, default=False)
+    # and/or
+    parser.add_argument('--row_color_mode', type=str, default='and')
 
     parser.add_argument('--profiler', type=bool_, default=False)
 
