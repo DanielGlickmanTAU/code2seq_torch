236ce45ca17fcf14d1e6440ecb3b83faa37e9fc3
danielglickman <glickman1@mail.tau.ac.il>
2022-08-16 17:07:29 +0300

    gps config

--- GraphGPS/runs/gps_baselines_config.py
+++ GraphGPS/runs/gps_baselines_config.py
@@ -56,6 +56,23 @@ def get_RWSE_gps_signnet_deepset_config():
     }
 
 
+def get_RWSE_gps_config(n_layers=6):
+    return {
+        'gt.layers': [n_layers],
+        'gt.n_layers_gnn_only': [0],
+        'posenc_LapPE.enable': [False],
+        'posenc_LapPE.layers': [0],
+        'dataset.node_encoder_name': 'TypeDictNode+RWSE',
+        'posenc_SignNet.enable': False,
+
+        'posenc_RWSE.enable': True,
+        'posenc_RWSE.kernel.times_func': 'range(1, 21)',
+        'posenc_RWSE.model': 'Linear',
+        'posenc_RWSE.dim_pe': 24,
+        'posenc_RWSE.raw_norm_type': "BatchNorm"
+    }
+
+
 def get_RWSE_GNN_config(layers=10):
     return {
         'optim.base_lr': [0.00003, 0.00005],
@@ -166,10 +183,10 @@ def get_STRONG_RSWE_gnn_transformer_signnet_AFTERGNN_deepset_config():
     }
 
 
-def get_nagasaki_config(total_layers=4, gnn_layers=2):
+def get_nagasaki_config(total_layers=4, gnn_layers=2, far_away=False):
     return {
         # 'optim.base_lr': [0.00003, 0.00005],
-        'nagasaki.steps': '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]',
+        'nagasaki.steps': '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18, 19, 20]' if far_away else '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]',
         'gt.layer_type': 'CustomGatedGCN+Nagasaki',
         'gt.layers': [total_layers],
         'gt.n_heads': [2],
