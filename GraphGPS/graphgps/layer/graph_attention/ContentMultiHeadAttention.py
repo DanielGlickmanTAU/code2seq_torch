4507fcc6073990ce5b3b2e696606ec731a833cb1
danielglickman <glickman1@mail.tau.ac.il>
2022-08-31 12:29:01 +0300

    extract attention class

--- GraphGPS/graphgps/layer/graph_attention/ContentMultiHeadAttention.py
+++ GraphGPS/graphgps/layer/graph_attention/ContentMultiHeadAttention.py
@@ -22,8 +22,7 @@ class ContentMultiheadAttention(torch.nn.Module):
     bias_v: Optional[torch.Tensor]
 
     def __init__(self, embed_dim, num_heads, dropout=0., bias=True, add_bias_kv=False,
-                 add_zero_attn=False,
-                 kdim=None, vdim=None, batch_first=False, device=None, dtype=None, use_distance_bias=False,
+                 kdim=None, vdim=None, batch_first=False, device=None, dtype=None,
                  ) -> None:
 
         factory_kwargs = {'device': device, 'dtype': dtype}
@@ -39,21 +38,8 @@ class ContentMultiheadAttention(torch.nn.Module):
         self.head_dim = embed_dim // num_heads
         assert self.head_dim * num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
 
-        if self._qkv_same_embed_dim is False:
-            self.q_proj_weight = Parameter(torch.empty((embed_dim, embed_dim), **factory_kwargs))
-            self.k_proj_weight = Parameter(torch.empty((embed_dim, self.kdim), **factory_kwargs))
-            self.v_proj_weight = Parameter(torch.empty((embed_dim, self.vdim), **factory_kwargs))
-            self.register_parameter('in_proj_weight', None)
-        else:
-            self.in_proj_weight = Parameter(torch.empty((3 * embed_dim, embed_dim), **factory_kwargs))
-            self.register_parameter('q_proj_weight', None)
-            self.register_parameter('k_proj_weight', None)
-            self.register_parameter('v_proj_weight', None)
-
-        if bias:
-            self.in_proj_bias = Parameter(torch.empty(3 * embed_dim, **factory_kwargs))
-        else:
-            self.register_parameter('in_proj_bias', None)
+        self.attention = ContentAttention(embed_dim, num_heads, bias=True, kdim=None, vdim=None, device=None,
+                                          dtype=None)
         self.out_proj = NonDynamicallyQuantizableLinear(embed_dim, embed_dim, bias=bias, **factory_kwargs)
 
         if add_bias_kv:
@@ -62,11 +48,7 @@ class ContentMultiheadAttention(torch.nn.Module):
         else:
             self.bias_k = self.bias_v = None
 
-        self.add_zero_attn = add_zero_attn
-
-        self.use_distance_bias = use_distance_bias
-
-        self.normalizer = AttentionWeightNormalizer(gating=False)
+        self.normalizer = AttentionWeightNormalizer(False)
 
         self._reset_parameters()
 
@@ -74,16 +56,7 @@ class ContentMultiheadAttention(torch.nn.Module):
             raise Exception('should not happen')
 
     def _reset_parameters(self):
-        if self._qkv_same_embed_dim:
-            xavier_uniform_(self.in_proj_weight)
-        else:
-            xavier_uniform_(self.q_proj_weight)
-            xavier_uniform_(self.k_proj_weight)
-            xavier_uniform_(self.v_proj_weight)
-
-        if self.in_proj_bias is not None:
-            constant_(self.in_proj_bias, 0.)
-            constant_(self.out_proj.bias, 0.)
+        constant_(self.out_proj.bias, 0.)
         if self.bias_k is not None:
             xavier_normal_(self.bias_k)
         if self.bias_v is not None:
@@ -95,8 +68,7 @@ class ContentMultiheadAttention(torch.nn.Module):
         if self.batch_first:
             query, key, value = [x.transpose(1, 0) for x in (query, key, value)]
 
-        q, k, v = _in_projection_packed(query, key, value, self.in_proj_weight, self.in_proj_bias)
-        attn = self._compute_attn(k, q, value)
+        attn, v = self.attention(query, key, value)
 
         attn_mask = pygraph_utils.reshape_attention_mask_to_multihead(attn_mask, self.num_heads)
         attn_output, attn_output_weights = multi_head_positional_attention(
@@ -110,6 +82,47 @@ class ContentMultiheadAttention(torch.nn.Module):
             return attn_output.transpose(1, 0), attn_output_weights
         else:
             return attn_output, attn_output_weights
+
+
+class ContentAttention(torch.nn.Module):
+    bias_k: Optional[torch.Tensor]
+    bias_v: Optional[torch.Tensor]
+
+    def __init__(self, embed_dim, num_heads, bias=True, kdim=None, vdim=None, device=None, dtype=None,
+                 ) -> None:
+
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(ContentAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.kdim = kdim if kdim is not None else embed_dim
+        self.vdim = vdim if vdim is not None else embed_dim
+        assert self.kdim == embed_dim and self.vdim == embed_dim
+
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        assert self.head_dim * num_heads == self.embed_dim, "embed_dim must be divisible by num_heads"
+
+        self.in_proj_weight = Parameter(torch.empty((3 * embed_dim, embed_dim), **factory_kwargs))
+        self.register_parameter('q_proj_weight', None)
+        self.register_parameter('k_proj_weight', None)
+        self.register_parameter('v_proj_weight', None)
+
+        if bias:
+            self.in_proj_bias = Parameter(torch.empty(3 * embed_dim, **factory_kwargs))
+        else:
+            self.register_parameter('in_proj_bias', None)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        xavier_uniform_(self.in_proj_weight)
+        if self.in_proj_bias is not None:
+            constant_(self.in_proj_bias, 0.)
+
+    def forward(self, query, key, value):
+        q, k, v = _in_projection_packed(query, key, value, self.in_proj_weight, self.in_proj_bias)
+        attn = self._compute_attn(k, q, value)
+        return attn, v
 
     def _compute_attn(self, k, q, value):
         tgt_len, bsz, embed_dim = value.shape
