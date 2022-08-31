a2c193e448c75edcc857b05a801861870d9f3533
danielglickman <glickman1@mail.tau.ac.il>
2022-08-31 08:08:24 +0300

    extract method

--- GraphGPS/main.py
+++ GraphGPS/main.py
@@ -140,6 +140,47 @@ def load_model(model, checkpoint_dir='runs'):
     cfg.run_dir = p
 
 
+def train_flow(cfg):
+    custom_set_run_dir(cfg, f'{start_time}_{run_id}')
+    set_printing()
+    cfg.dataset.split_index = split_index
+    cfg.seed = seed
+    cfg.run_id = run_id
+    seed_everything(cfg.seed)
+    auto_select_device()
+    if cfg.train.finetune:
+        cfg = load_pretrained_model_cfg(cfg)
+    logging.info(f"[*] Run ID {run_id}: seed={cfg.seed}, "
+                 f"split_index={cfg.dataset.split_index}")
+    logging.info(f"    Starting now: {datetime.datetime.now()}")
+    # Set machine learning pipeline
+    loaders = create_loader()
+    loggers = create_logger()
+    model = create_model()
+    if cfg.train.finetune:
+        model = init_model_from_pretrained(model, cfg.train.finetune,
+                                           cfg.train.freeze_pretrained)
+    optimizer = create_optimizer(model.parameters(),
+                                 new_optimizer_config(cfg))
+    scheduler = create_scheduler(optimizer, new_scheduler_config(cfg))
+    if cfg.load_checkpoint_from_dir:
+        load_model(model, checkpoint_dir=cfg.load_checkpoint_from_dir)
+    # Print model info
+    logging.info(model)
+    logging.info(cfg)
+    cfg.params = params_count(model)
+    logging.info('Num parameters: {}'.format(cfg.params))
+    # Start training
+    if cfg.train.mode == 'standard':
+        if cfg.wandb.use:
+            logging.warning("[W] WandB logging is not supported with the "
+                            "default train.mode, set it to `custom`")
+        train(loggers, loaders, model, optimizer, scheduler)
+    else:
+        train_dict[cfg.train.mode](loggers, loaders, model, optimizer,
+                                   scheduler)
+
+
 if __name__ == '__main__':
     # Load cmd line args
     args = parse_args()
@@ -161,8 +202,6 @@ if __name__ == '__main__':
 
     cfg_assertions(cfg)
 
-    # custom_set_out_dir(cfg, args.cfg_file, cfg.name_tag)
-    # dump_cfg(cfg)
 
     # Set Pytorch environment
     torch.set_num_threads(cfg.num_threads)
@@ -171,47 +210,7 @@ if __name__ == '__main__':
     start_time = time.time()
     for run_id, seed, split_index in zip(*run_loop_settings()):
         # Set configurations for each run
-        custom_set_run_dir(cfg, f'{start_time}_{run_id}')
-        set_printing()
-        cfg.dataset.split_index = split_index
-        cfg.seed = seed
-        cfg.run_id = run_id
-        seed_everything(cfg.seed)
-        auto_select_device()
-        if cfg.train.finetune:
-            cfg = load_pretrained_model_cfg(cfg)
-        logging.info(f"[*] Run ID {run_id}: seed={cfg.seed}, "
-                     f"split_index={cfg.dataset.split_index}")
-        logging.info(f"    Starting now: {datetime.datetime.now()}")
-        # Set machine learning pipeline
-        loaders = create_loader()
-        loggers = create_logger()
-
-        model = create_model()
-
-        if cfg.train.finetune:
-            model = init_model_from_pretrained(model, cfg.train.finetune,
-                                               cfg.train.freeze_pretrained)
-        optimizer = create_optimizer(model.parameters(),
-                                     new_optimizer_config(cfg))
-        scheduler = create_scheduler(optimizer, new_scheduler_config(cfg))
-        if cfg.load_checkpoint_from_dir:
-            load_model(model, checkpoint_dir=cfg.load_checkpoint_from_dir)
-
-        # Print model info
-        logging.info(model)
-        logging.info(cfg)
-        cfg.params = params_count(model)
-        logging.info('Num parameters: {}'.format(cfg.params))
-        # Start training
-        if cfg.train.mode == 'standard':
-            if cfg.wandb.use:
-                logging.warning("[W] WandB logging is not supported with the "
-                                "default train.mode, set it to `custom`")
-            train(loggers, loaders, model, optimizer, scheduler)
-        else:
-            train_dict[cfg.train.mode](loggers, loaders, model, optimizer,
-                                       scheduler)
+        train_flow(cfg)
     # Aggregate results from different seeds
     # agg_runs(cfg.out_dir, cfg.metric_best)
     # When being launched in batch mode, mark a yaml as done
