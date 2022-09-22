774f15c77e662a2dcdf4609843136ae233a3b516
danielglickman <glickman1@mail.tau.ac.il>
2022-09-22 21:55:57 +0300

    res-mlp

--- GraphGPS/graphgps/layer/graph_attention/positional/positional_attention_weight.py
+++ GraphGPS/graphgps/layer/graph_attention/positional/positional_attention_weight.py
@@ -50,6 +50,16 @@ class AdjStackAttentionWeights(torch.nn.Module):
 
             layers.append(torch.nn.Linear(input_dim, dim_out))
             self.weight = torch.nn.Sequential(*layers)
+        elif ffn == 'res-net':
+            layers = [torch.nn.BatchNorm1d(input_dim)]
+            for _ in range(ffn_layers):
+                linear_bn = [torch.nn.Linear(input_dim, hidden_dim), torch.nn.BatchNorm1d(hidden_dim), torch.nn.ReLU(),
+                             torch.nn.Linear(hidden_dim, input_dim), torch.nn.BatchNorm1d(input_dim)]
+                layers.append(Residual(torch.nn.Sequential(*linear_bn)))
+                layers.append(torch.nn.ReLU())
+
+            layers.append(torch.nn.Linear(input_dim, dim_out))
+            self.weight = torch.nn.Sequential(*layers)
 
         elif ffn == 'linear':
             self.weight = nn.Linear(in_features=input_dim, out_features=dim_out)
