dc23e034a6254de9269d57b7eff234e5c2805ea9
danielglickman <glickman1@mail.tau.ac.il>
2022-11-23 16:30:25 +0200

    extract class

--- GraphGPS/graphgps/network/gps_model.py
+++ GraphGPS/graphgps/network/gps_model.py
@@ -2,59 +2,12 @@ import torch
 import torch_geometric.graphgym.register as register
 from torch_geometric.graphgym.config import cfg
 from torch_geometric.graphgym.models.gnn import GNNPreMP
-from torch_geometric.graphgym.models.layer import (new_layer_config,
-                                                   BatchNorm1dNode)
 from torch_geometric.graphgym.register import register_network
 
 from graphgps.layer.gps_layer import GPSLayer
 from graphgps.layer.graph_attention.positional.cls import CLSNode, CLSHead
 from graphgps.layer.graph_attention.positional.positional_attention_weight import Diffuser
-
-
-class FeatureEncoder(torch.nn.Module):
-    """
-    Encoding node and edge features
-
-    Args:
-        dim_in (int): Input feature dimension
-    """
-
-    def __init__(self, dim_in, node_encoder_name=None,
-                 edge_encoder_name=None, contract=False):
-        super(FeatureEncoder, self).__init__()
-        if node_encoder_name is None:
-            node_encoder_name = cfg.dataset.node_encoder_name
-        if edge_encoder_name is None:
-            edge_encoder_name = cfg.dataset.edge_encoder_name
-        self.dim_in = dim_in
-        if cfg.dataset.node_encoder:
-            # Encode integer node features via nn.Embeddings
-            NodeEncoder = register.node_encoder_dict[
-                node_encoder_name]
-            self.node_encoder = NodeEncoder(cfg.gnn.dim_inner, expand_x='contract') if contract else NodeEncoder(
-                cfg.gnn.dim_inner)
-            if cfg.dataset.node_encoder_bn:
-                self.node_encoder_bn = BatchNorm1dNode(
-                    new_layer_config(cfg.gnn.dim_inner, -1, -1, has_act=False,
-                                     has_bias=False, cfg=cfg))
-            # Update dim_in to reflect the new dimension fo the node features
-            self.dim_in = cfg.gnn.dim_inner
-        if cfg.dataset.edge_encoder and edge_encoder_name:
-            # Hard-set edge dim for PNA.
-            cfg.gnn.dim_edge = 16 if 'PNA' in cfg.gt.layer_type else cfg.gnn.dim_inner
-            # Encode integer edge features via nn.Embeddings
-            EdgeEncoder = register.edge_encoder_dict[
-                edge_encoder_name]
-            self.edge_encoder = EdgeEncoder(cfg.gnn.dim_edge)
-            if cfg.dataset.edge_encoder_bn:
-                self.edge_encoder_bn = BatchNorm1dNode(
-                    new_layer_config(cfg.gnn.dim_edge, -1, -1, has_act=False,
-                                     has_bias=False, cfg=cfg))
-
-    def forward(self, batch):
-        for module in self.children():
-            batch = module(batch)
-        return batch
+from graphgps.network.gps_feature_encoder import FeatureEncoder
 
 
 class GPSModel(torch.nn.Module):
